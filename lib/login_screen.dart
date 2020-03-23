18c545a56abf229f6c7371acfa635d5588e1f0bb
Vansh Goel <vanshg395@gmail.com>
2020-03-23 18:00:55 +0530

    added login screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,284 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+// import './providers/auth.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool visisblePassword = false;
+  bool _isLoading = false;
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  String errorMessage = '';
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passController = TextEditingController();
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    final email = _authData['email'];
+    final password = _authData['password'];
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      // await Provider.of<Auth>(context, listen: false).login(email, password);
+    } catch (error) {
+      errorMessage = error.toString();
+      setState(() {
+        _emailController.text = '';
+        _passController.text = '';
+      });
+      showDialog(
+        context: context,
+        builder: (context) => Platform.isIOS
+            ? CupertinoAlertDialog(
+                title: Text('Authentication Error'),
+                content: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(errorMessage),
+                ),
+                actions: <Widget>[
+                  CupertinoDialogAction(
+                    child: Text('OK'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              )
+            : AlertDialog(
+                backgroundColor: Colors.grey,
+                title: Text('Authentication Error'),
+                content: Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(errorMessage),
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('OK'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+      );
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: Stack(
+          children: <Widget>[
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 0.3,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.4),
+                              offset: Offset(1.0, 6.0),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(80),
+                        ),
+                        height: 150,
+                        width: 150,
+                        child: Image.asset(
+                          'assets/logo/ashwa-colored.png',
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Welcome to FitKnees!',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  labelText: 'Username',
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  errorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  focusedErrorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                keyboardAppearance: Brightness.light,
+                                validator: (val) {
+                                  if (val == '') {
+                                    return 'This Field is required.';
+                                  }
+                                },
+                                onSaved: (val) {
+                                  _authData['email'] = val;
+                                },
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              TextFormField(
+                                controller: _passController,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      visisblePassword
+                                          ? FontAwesomeIcons.eye
+                                          : FontAwesomeIcons.eyeSlash,
+                                      color: Colors.grey,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        visisblePassword = !visisblePassword;
+                                      });
+                                    },
+                                  ),
+                                  labelText: 'Password',
+                                  labelStyle: TextStyle(color: Colors.grey),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  errorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  focusedErrorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.blue),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+                                  ),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                keyboardAppearance: Brightness.light,
+                                obscureText: !visisblePassword,
+                                validator: (val) {
+                                  if (val == '') {
+                                    return 'This Field is required.';
+                                  }
+                                },
+                                onSaved: (val) {
+                                  _authData['password'] = val;
+                                },
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.6,
+                                height: 40,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(50),
+                                  child: _isLoading
+                                      ? Center(
+                                          child: CircularProgressIndicator(
+                                            valueColor: AlwaysStoppedAnimation(
+                                              Colors.grey,
+                                            ),
+                                          ),
+                                        )
+                                      : RaisedButton(
+                                          color: Colors.grey[350],
+                                          textColor: Colors.black,
+                                          child: Text(
+                                            'LOGIN',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontFamily: 'SFProTextSemiMed',
+                                            ),
+                                          ),
+                                          // onPressed: _submit,
+                                          onPressed: () {},
+                                        ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 50,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text('Don\'t have an account?'),
+                                  FlatButton(
+                                    child: Text(
+                                      'SIGNUP HERE',
+                                      style:
+                                          TextStyle(color: Color(0xFF607EEA)),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              ),
+                              FlatButton(
+                                child: Text(
+                                  'FORGOT PASSWORD',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
