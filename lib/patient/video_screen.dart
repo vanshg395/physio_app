dcdb60c8ce680eb1472aed7a0f4f026c1b0361d9
Vansh Goel <vanshg395@gmail.com>
2020-03-26 02:26:36 +0530

    added video exercise for patient

---
+++ lib/patient/video_screen.dart
@@ -0,0 +1,62 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoScreen extends StatefulWidget {
+  VideoScreen(this.title, this.vidUrl);
+
+  final String title;
+  final String vidUrl;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoScreenState();
+  }
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  VideoPlayerController _videoPlayerController1;
+  VideoPlayerController _videoPlayerController2;
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController1 = VideoPlayerController.network(widget.vidUrl);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController1,
+      aspectRatio: 3 / 2,
+      autoPlay: true,
+      looping: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController1.dispose();
+    _videoPlayerController2.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Chewie(
+                controller: _chewieController,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
