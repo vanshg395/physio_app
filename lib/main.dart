efed0e46f6eca20cfd04f23a8d2959744bc63c50
Sandeep <rayanuthalas@gmail.com>
2020-03-28 19:04:49 +0530

    checvk

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,9 @@ import './patient/router.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
